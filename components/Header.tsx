4354848f8b28379785c6d29cbf4661e60ad34668
Alia Alamer <aliaalamer6582@gmail.com>
2026-06-18 17:08:56 +0200

    implement default header variant

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,27 +2,58 @@ import Image from "next/image";
 import Link from "next/link";
 
 type HeaderProps = {
-    variant?: "home" | "welcome";
+    variant?: "home" | "welcome" | "default";
 };
 
 export default function Header({ variant = "home" }: HeaderProps) {
-    if (variant === "welcome") {
+
+    if (variant === "default") {
         return (
             <header className="relative bg-[#008CEA] px-6 py-4">
-                <h1 className="text-center text-2xl font-bold text-white">
-                    Willkommen zurück
-                </h1>
-                <h2 className="text-center">
-                    ÖVSV Lernkurs
-                    </h2>
+                <div className="flex flex-col items-center">
+                    <h1 className="text-2xl font-bold text-white">
+                        Lernkurs
+                    </h1>
+
+                    <p className="text-sm text-white/80">
+                        ÖVSV Lernkurs
+                    </p>
+                </div>
 
                 <Link href="/">
                     <Image
                         src="/logoWhite.png"
                         alt="ÖVSV Lernkurs Logo"
-                        width={32}
-                        height={32}
-                        className="absolute right-6 top-1/2 h-10 w-6 -translate-y-1/2"
+                        width={30}
+                        height={30}
+                        className="absolute right-6 top-1/2 -translate-y-1/2"
+                        priority
+                    />
+                </Link>
+            </header>
+        );
+    }
+
+    if (variant === "welcome") {
+        return (
+            <header className="relative bg-[#008CEA] px-6 py-4">
+                <div className="flex flex-col items-center">
+                    <h1 className="text-2xl font-bold text-white">
+                        Willkommen zurück
+                    </h1>
+
+                    <p className="text-sm text-white/80">
+                        ÖVSV Lernkurs
+                    </p>
+                </div>
+
+                <Link href="/">
+                    <Image
+                        src="/logoWhite.png"
+                        alt="ÖVSV Lernkurs Logo"
+                        width={30}
+                        height={30}
+                        className="absolute right-6 top-1/2 -translate-y-1/2"
                         priority
                     />
                 </Link>
