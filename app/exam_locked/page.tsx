0589ca2271dc485ab19fb72ff2db199643c3e80f
Alia Alamer <aliaalamer6582@gmail.com>
2026-06-21 20:47:13 +0200

    add guest exam page layout with header and footer nav

---
+++ app/exam_locked/page.tsx
@@ -0,0 +1,16 @@
+import Header from "@/components/Header";
+import FooterNav from "@/components/FooterNav";
+
+export default function ExamGuestPage() {
+    return (
+        <div className="flex min-h-screen flex-col bg-white">
+            <Header variant="default" />
+
+            <main className="flex flex-1 flex-col items-center justify-center px-6 pb-24 text-center">
+                
+            </main>
+
+            <FooterNav />
+        </div>
+    );
+}
