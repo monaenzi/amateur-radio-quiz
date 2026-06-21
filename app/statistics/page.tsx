1030f5a13b20b2ccee6e3140b983cb94fe3e9a79
Alia Alamer <aliaalamer6582@gmail.com>
2026-06-21 23:31:29 +0200

    add page layout with header and footer nav for statistics screen

---
+++ app/statistics/page.tsx
@@ -0,0 +1,18 @@
+import Header from "@/components/Header";
+import FooterNav from "@/components/FooterNav";
+
+export default function StatistikPage() {
+    return (
+        <main className="min-h-screen bg-white md:p-8">
+            <div className="w-full bg-white md:mx-auto md:max-w-7xl">
+                <Header variant="default" />
+
+                <div className="px-6 pb-24 pt-6">
+                    
+                </div>
+
+                <FooterNav />
+            </div>
+        </main>
+    );
+}
